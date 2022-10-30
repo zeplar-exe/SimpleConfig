0a600f8c5a584f07895822b3d9d221831b5d6f04
Zander Baker <zanderbaker100@gmail.com>
2022-10-29 18:12:54 -0700

    Add boolean parser

---
+++ SimpleConfig/ValueParsers/BooleanConfigParser.cs
@@ -0,0 +1,13 @@
+﻿namespace SimpleConfig.ValueParsers;
+
+public class BooleanConfigParser : ConfigValueParser
+{
+    public override bool TryParse(string input, out object? output)
+    {
+        var result = bool.TryParse(input, out var o);
+
+        output = o;
+
+        return result;
+    }
+}
