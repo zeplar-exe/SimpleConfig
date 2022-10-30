c4b62cb5248aef7bd8d1a342afef9c47b312cc2d
Zander Baker <zanderbaker100@gmail.com>
2022-10-29 18:14:07 -0700

    Char primitive parser

---
+++ SimpleConfig/ValueParsers/CharConfigParser.cs
@@ -0,0 +1,18 @@
+﻿namespace SimpleConfig.ValueParsers;
+
+public class CharConfigParser : ConfigValueParser
+{
+    public override bool TryParse(string input, out object? output)
+    {
+        output = default;
+        
+        if (input.Length == 1)
+        {
+            output = input[0];
+            
+            return true;
+        }
+
+        return false;
+    }
+}
